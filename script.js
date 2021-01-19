cbb259f7d7aa25603122d6bebd06b9c9611bba36
Vlad Zankov <72306942+VladZankov@users.noreply.github.com>
2021-01-19 20:04:49 +0300

    Clear script.js

--- script.js
+++ script.js
@@ -7,15 +7,9 @@ addButton.addEventListener('click', () => {
     else {
         tasks.push({taskToDo: task.value, completed: false});
         let elemOfList = document.createElement('li');
-        /* let checkBox = document.createElement('input').setAttribute('type', 'checkbox') */;
         elemOfList.innerHTML = `<input type='checkbox' class='checkbox'> ${task.value}`;
         listOfTasks.append(elemOfList);
-    }
+    };
 })
 
-/* tasks.forEach((item, index, arr)=> {
-    let elemOfList = document.createElement('li');
-    let checkBox = document.createElement('input').setAttribute(type, checkbox);
-    elemOfList.innerHTML = checkBox + arr[index].taskToDo;
-    listOfTasks.append(elemOfList);
-}) */
+
