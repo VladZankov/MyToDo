7426b13d65a6944657de47639784d4ed1075935b
Vlad Zankov <72306942+VladZankov@users.noreply.github.com>
2021-02-02 20:13:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 let task = document.getElementById('input');
 let addButton = document.getElementById('add')
-let tasks = [];
 let listOfTasks = document.getElementById('list_of_tasks');
 addButton.addEventListener('click', () => {
     if (!task.value) {alert("Empty task!")}
     else {
-        tasks.push({taskToDo: task.value, completed: false});
         let elemOfList = document.createElement('li');
         elemOfList.innerHTML = `<input type='checkbox' class='checkbox'> ${task.value}`;
         listOfTasks.append(elemOfList);
